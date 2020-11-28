276cb0b20df9f587db763137632bf083839f9ba9
Matthias Seitz <matthias.seitz@outlook.de>
2020-11-28 23:06:17 +0100

    add manager sketches

---
+++ src/handler/emulation.rs
@@ -0,0 +1,51 @@
+use chromeoxid_types::Method;
+
+use crate::cdp::browser_protocol::emulation::{
+    ScreenOrientation, ScreenOrientationType, SetDeviceMetricsOverrideParams,
+    SetTouchEmulationEnabledParams,
+};
+use crate::handler::cmd::CommandChain;
+use crate::handler::viewport::Viewport;
+
+#[derive(Debug, Default)]
+pub struct EmulationManager {
+    pub emulating_mobile: bool,
+    pub has_touch: bool,
+    pub needs_reload: bool,
+}
+
+impl EmulationManager {
+    pub fn init_commands(&mut self, viewport: &Viewport) -> CommandChain {
+        let orientation = if viewport.is_landscape {
+            ScreenOrientation::new(ScreenOrientationType::LandscapePrimary, 90)
+        } else {
+            ScreenOrientation::new(ScreenOrientationType::PortraitPrimary, 0)
+        };
+
+        let set_device = SetDeviceMetricsOverrideParams::builder()
+            .mobile(viewport.is_mobile)
+            .width(viewport.width)
+            .height(viewport.height)
+            .device_scale_factor(viewport.device_scale_factor.unwrap_or(1.))
+            .screen_orientation(orientation)
+            .build()
+            .unwrap();
+
+        let set_touch = SetTouchEmulationEnabledParams::new(true);
+
+        let chain = CommandChain::new(vec![
+            (
+                set_device.identifier(),
+                serde_json::to_value(set_device).unwrap(),
+            ),
+            (
+                set_touch.identifier(),
+                serde_json::to_value(set_touch).unwrap(),
+            ),
+        ]);
+
+        self.needs_reload =
+            self.emulating_mobile != viewport.is_mobile || self.has_touch != viewport.has_touch;
+        chain
+    }
+}
---
+++ src/handler/network.rs
@@ -0,0 +1,39 @@
+use chromeoxid_types::Method;
+
+use crate::cdp::browser_protocol::{
+    network::EnableParams, security::SetIgnoreCertificateErrorsParams,
+};
+use crate::handler::cmd::CommandChain;
+
+#[derive(Debug)]
+pub struct NetworkManager {
+    ignore_httpserrors: bool,
+}
+
+impl NetworkManager {
+    pub fn new(ignore_httpserrors: bool) -> Self {
+        Self { ignore_httpserrors }
+    }
+
+    pub fn init_commands(&self) -> CommandChain {
+        let enable = EnableParams::default();
+        if self.ignore_httpserrors {
+            let ignore = SetIgnoreCertificateErrorsParams::new(true);
+            CommandChain::new(vec![
+                (enable.identifier(), serde_json::to_value(enable).unwrap()),
+                (ignore.identifier(), serde_json::to_value(ignore).unwrap()),
+            ])
+        } else {
+            CommandChain::new(vec![(
+                enable.identifier(),
+                serde_json::to_value(enable).unwrap(),
+            )])
+        }
+    }
+}
+
+impl Default for NetworkManager {
+    fn default() -> Self {
+        NetworkManager::new(true)
+    }
+}
