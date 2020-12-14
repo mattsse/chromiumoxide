6b880958115f7f021fc865a8b8ece257cd87dab4
Matthias Seitz <matthias.seitz@outlook.de>
2020-12-14 23:08:10 +0100

    feat: add tokio example

---
+++ examples/wiki-tokio.rs
@@ -0,0 +1,31 @@
+use futures::StreamExt;
+
+use chromiumoxide::browser::{Browser, BrowserConfig};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (browser, mut handler) =
+        Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
+
+    let handle = tokio::task::spawn(async move {
+        loop {
+            let _event = handler.next().await.unwrap();
+        }
+    });
+
+    let page = browser.new_page("https://en.wikipedia.org").await?;
+
+    page.find_element("input#searchInput")
+        .await?
+        .click()
+        .await?
+        .type_str("Rust programming language")
+        .await?
+        .press_key("Enter")
+        .await?;
+
+    let _html = page.wait_for_navigation().await?.content().await?;
+
+    handle.await?;
+    Ok(())
+}
