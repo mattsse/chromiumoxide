a89784ff5c12c38d27c71c22fbe5b10dd9443200
Matthias Seitz <matthias.seitz@outlook.de>
2020-12-21 23:03:27 +0100

    feat: make ExceptionDetails an error type

--- chromiumoxide_cdp/src/lib.rs
+++ chromiumoxide_cdp/src/lib.rs
@@ -1,5 +1,8 @@
+use std::fmt;
+
 use crate::cdp::browser_protocol::network::{CookieParam, DeleteCookiesParams};
 use crate::cdp::browser_protocol::target::CreateTargetParams;
+use crate::cdp::js_protocol::runtime::{ExceptionDetails, StackTrace};
 
 // Include all the types
 include!(concat!(env!("OUT_DIR"), "/cdp.rs"));
@@ -20,5 +23,38 @@ impl DeleteCookiesParams {
             domain: param.domain.clone(),
             path: param.path.clone(),
         }
+    }
+}
+
+impl fmt::Display for ExceptionDetails {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(
+            f,
+            "{}:{}: {}",
+            self.line_number, self.column_number, self.text
+        )?;
+
+        if let Some(stack) = self.stack_trace.as_ref() {
+            stack.fmt(f)?
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for ExceptionDetails {}
+
+impl fmt::Display for StackTrace {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(desc) = self.description.as_ref() {
+            writeln!(f, "{}", desc)?;
+        }
+        for frame in &self.call_frames {
+            writeln!(
+                f,
+                "{}@{}:{}:{}",
+                frame.function_name, frame.url, frame.line_number, frame.column_number
+            )?;
+        }
+        Ok(())
     }
 }
