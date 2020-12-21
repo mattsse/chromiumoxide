0e6e196632908608efa6342da0dd4120550136fd
Matthias Seitz <matthias.seitz@outlook.de>
2020-12-21 23:07:33 +0100

    chore(clippy): make clippy happy

--- examples/incognito.rs
+++ examples/incognito.rs
@@ -4,7 +4,6 @@ use chromiumoxide::browser::{Browser, BrowserConfig};
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let (mut browser, mut handler) =
         Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
 
