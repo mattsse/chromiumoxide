693485b86af05403f4ceea8256c27004910fba12
Emile Fugulin <code@efugulin.com>
2022-12-27 12:12:12 -0500

    add context messages and check status code

--- chromiumoxide_fetcher/src/browser/runtime.rs
+++ chromiumoxide_fetcher/src/browser/runtime.rs
@@ -1,5 +1,7 @@
 use std::path::{Path, PathBuf};
 
+use anyhow::Context;
+
 use super::ZipArchive;
 
 pub struct BrowserFetcherRuntime;
@@ -12,17 +14,29 @@ impl BrowserFetcherRuntime {
 
     pub async fn download(url: &str, archive_path: &Path) -> anyhow::Result<()> {
         use async_std::io::WriteExt;
+        use surf::http;
 
         // Open file
-        let file = async_std::fs::File::create(&archive_path).await?;
+        let file = async_std::fs::File::create(&archive_path)
+            .await
+            .context("Failed to create archive file")?;
         let mut file = async_std::io::BufWriter::new(file);
 
         // Download
-        let res = surf::get(url).await.map_err(|e| e.into_inner())?;
-        async_std::io::copy(res, &mut file).await?;
+        let url = url.parse::<surf::Url>().context("Invalid archive url")?;
+        let res = surf::RequestBuilder::new(http::Method::Get, url)
+            .await
+            .map_err(|e| e.into_inner())
+            .context("Failed to send request to host")?;
+        if res.status() != surf::StatusCode::Ok {
+            anyhow::bail!("Invalid archive url");
+        }
+        async_std::io::copy(res, &mut file)
+            .await
+            .context("Failed to write to archive file")?;
 
         // Flush to disk
-        file.flush().await?;
+        file.flush().await.context("Failed to flush to disk")?;
         Ok(())
     }
 
@@ -42,17 +56,27 @@ impl BrowserFetcherRuntime {
         use tokio::io::AsyncWriteExt;
 
         // Open file
-        let file = tokio::fs::File::create(&archive_path).await?;
+        let file = tokio::fs::File::create(&archive_path)
+            .await
+            .context("Failed to create archive file")?;
         let mut file = tokio::io::BufWriter::new(file);
 
         // Download
-        let mut response = reqwest::get(url).await?;
-        while let Some(chunk) = response.chunk().await? {
-            file.write(&chunk).await?;
+        let url = url.parse::<reqwest::Url>().context("Invalid archive url")?;
+        let mut res = reqwest::get(url)
+            .await
+            .context("Failed to send request to host")?;
+        if res.status() != reqwest::StatusCode::OK {
+            anyhow::bail!("Invalid archive url");
+        }
+        while let Some(chunk) = res.chunk().await.context("Failed to read response chunk")? {
+            file.write(&chunk)
+                .await
+                .context("Failed to write to archive file")?;
         }
 
         // Flush to disk
-        file.flush().await?;
+        file.flush().await.context("Failed to flush to disk")?;
 
         Ok(())
     }
@@ -66,11 +90,11 @@ fn do_unzip(archive_path: &Path, folder_path: &Path) -> anyhow::Result<()> {
     use std::fs;
 
     // Prepare
-    fs::create_dir_all(folder_path)?;
-    let file = fs::File::open(archive_path)?;
+    fs::create_dir_all(folder_path).context("Failed to create folder")?;
+    let file = fs::File::open(archive_path).context("Failed to open archive")?;
 
     // Unzip
-    let mut archive = ZipArchive::new(file)?;
+    let mut archive = ZipArchive::new(file).context("Failed to unzip archive")?;
     archive.extract(folder_path)?;
 
     // Clean (if possible)
