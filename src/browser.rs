30ec6633ca9af250674ef39ae9c70bc1089cb121
j-mendez <jeff@a11ywatch.com>
2023-09-12 15:26:41 -0400

    chore(connect): use http get default

--- src/browser.rs
+++ src/browser.rs
@@ -81,7 +81,7 @@ impl Browser {
 
         if debug_ws_url.starts_with("http") {
             let req =  reqwest::Client::new()
-            .post(if debug_ws_url.ends_with("/json/version") { debug_ws_url.to_string() } else { format!("{}{}json/version", &debug_ws_url, if debug_ws_url.ends_with("/") { "" } else { "/" }) })
+            .get(if debug_ws_url.ends_with("/json/version") { debug_ws_url.to_string() } else { format!("{}{}json/version", &debug_ws_url, if debug_ws_url.ends_with("/") { "" } else { "/" }) })
             .header("content-type", "application/json")
             .send()
             .await
