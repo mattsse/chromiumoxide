6968e92e736f86662b972d0083c4b1ae393afd1c
Nathan Mossaad <49043384+Nathan-Mossaad@users.noreply.github.com>
2024-08-12 16:37:53 +0200

    Storage cookie functions (#233)
    * Added Storage cookie functions
    
    See: https://chromedevtools.github.io/devtools-protocol/tot/Storage/
    
    * Added Storage cookie example
    
    * rustfmt
    
    ---------
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- src/browser.rs
+++ src/browser.rs
@@ -11,6 +11,10 @@ use futures::channel::oneshot::channel as oneshot_channel;
 use futures::select;
 use futures::SinkExt;
 
+use chromiumoxide_cdp::cdp::browser_protocol::network::{Cookie, CookieParam};
+use chromiumoxide_cdp::cdp::browser_protocol::storage::{
+    ClearCookiesParams, GetCookiesParams, SetCookiesParams,
+};
 use chromiumoxide_cdp::cdp::browser_protocol::target::{
     CreateBrowserContextParams, CreateTargetParams, DisposeBrowserContextParams, TargetId,
     TargetInfo,
@@ -479,6 +483,33 @@ impl Browser {
             .await?;
 
         Ok(())
+    }
+
+    /// Clears cookies.
+    pub async fn clear_cookies(&self) -> Result<()> {
+        self.execute(ClearCookiesParams::default()).await?;
+        Ok(())
+    }
+
+    /// Returns all browser cookies.
+    pub async fn get_cookies(&self) -> Result<Vec<Cookie>> {
+        Ok(self
+            .execute(GetCookiesParams::default())
+            .await?
+            .result
+            .cookies)
+    }
+
+    /// Sets given cookies.
+    pub async fn set_cookies(&self, mut cookies: Vec<CookieParam>) -> Result<&Self> {
+        for cookie in &mut cookies {
+            if let Some(url) = cookie.url.as_ref() {
+                crate::page::validate_cookie_url(url)?;
+            }
+        }
+
+        self.execute(SetCookiesParams::new(cookies)).await?;
+        Ok(self)
     }
 }
 
